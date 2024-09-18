4c51607aa9928e96e6d92f0e1fb18ddf1744747b
mikailakar <huseyin.akar@bil.omu.edu.tr>
2024-09-18 20:30:02 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { DictionaryService } from './dictionary.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontendProject';
+  word: string = '';
+  wordDetails: any;
+
+  constructor(private dictionaryService: DictionaryService) { }
+
+  searchWord() {
+    if (this.word.trim()) {
+      this.dictionaryService.getWordDetails(this.word).subscribe(
+        (data) => {
+          this.wordDetails = data;
+        },
+        (error) => {
+          console.error('Error fetching word details', error);
+          this.wordDetails = null;
+        }
+      );
+    }
+  }
 }
