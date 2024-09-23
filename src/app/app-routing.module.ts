171343ee58536e96caf31897ad378bc3ee61d55d
mikailakar <huseyin.akar@bil.omu.edu.tr>
2024-09-24 01:09:32 +0300

    api_access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'en/:word', component: AppComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { DictionaryService } from './dictionary.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   word: string = '';
   wordDetails: any;
 
-  constructor(private dictionaryService: DictionaryService) { }
+  constructor(
+    private router: Router,
+    //private route: ActivatedRoute,
+    private dictionaryService: DictionaryService
+  ) {}
 
-  searchWord() {
-    if (this.word.trim()) {
-      this.dictionaryService.getWordDetails(this.word).subscribe(
-        (data) => {
-          this.wordDetails = data;
-        },
-        (error) => {
-          console.error('Error fetching word details', error);
-          this.wordDetails = null;
-        }
-      );
+  ngOnInit(): void {
+    //console.log(this.location.path());//Ang
+    //console.log(window.location.href, this.router);//JS
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(() => {
+      const match = this.router.url.match(/^\/en\/(.+)/);
+      const searchTerm = match ? match[1] : "";
+      if (searchTerm) {
+        this.word = searchTerm;
+        this.performSearch(this.word);
+      }else if (searchTerm == "") {
+        this.word = '';
+        this.wordDetails = null;
+      }
+    });
+  }
+
+  searchWord(word?: string): void {
+    const searchTerm = word || this.word;
+    if (searchTerm.trim()) {
+      this.router.navigate(['/en', searchTerm])/*.then(() => {
+        this.word = searchTerm;
+        this.performSearch(searchTerm);
+      })*/;
     }
+  }
+
+  performSearch(word: string): void {
+    this.dictionaryService.getWordDetails(word).subscribe(
+      (data) => {
+        this.wordDetails = data;
+      },
+      (error) => {
+        console.error('Error fetching word details', error);
+        this.wordDetails = null;
+      }
+    );
   }
 }
