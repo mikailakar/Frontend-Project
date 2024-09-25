24059042c5e3cc9f03a24e8414fac2e9e9795bc4
mikailakar <huseyin.akar@bil.omu.edu.tr>
2024-09-25 16:06:01 +0300

    frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { DictionaryService } from './dictionary.service';
 import { filter } from 'rxjs/operators';
@@ -12,6 +12,8 @@ export class AppComponent implements OnInit {
   word: string = '';
   wordDetails: any;
 
+  @ViewChild('audioPlayer', { static: false }) audioPlayer!: ElementRef<HTMLAudioElement>;
+
   constructor(
     private router: Router,
     private dictionaryService: DictionaryService
@@ -24,8 +26,8 @@ export class AppComponent implements OnInit {
       const match = this.router.url.match(/^\/en\/(.+)/);
       const searchTerm = match ? match[1] : '';
       if (searchTerm) {
-        this.word = searchTerm;
-        this.performSearch(this.word);
+        this.word = searchTerm.replace('%20', ' ');
+        this.performSearch(searchTerm);
       }else if (searchTerm == '') {
         this.word = '';
         this.wordDetails = null;
@@ -50,5 +52,36 @@ export class AppComponent implements OnInit {
         this.wordDetails = null;
       }
     );
+  }
+
+  hasSynonyms(): boolean {
+    if (!this.wordDetails || !this.wordDetails.length) {
+      return false;
+    }
+
+    // Check if any meanings contain synonyms
+    for (let detail of this.wordDetails) {
+      for (let meaning of detail.meanings) {
+        if (meaning.synonyms && meaning.synonyms.length > 0) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  getIndex(i: number, j: number): number {
+    let counter = 0;
+    for (let k = 0; k < i; k++) {
+      counter += this.wordDetails[k].meanings.length;
+    }
+    return counter + j + 1;
+  }
+
+  playAudio(): void {
+    if (this.audioPlayer) {
+      this.audioPlayer.nativeElement.play();
+    }
   }
 }
